53cd1eef742dc35540fbb52ed0a7cdb26f481afc
joaocarloshespanhol <joaocarlos.hespanhol@hotmail.com>
2022-11-30 13:10:11 -0300

    Project Question Game

--- components/Temporizador.tsx
+++ components/Temporizador.tsx
@@ -11,18 +11,7 @@ interface TemporizadorProps {
 export default function Temporizador(props: TemporizadorProps) {
     return (
         <div className={styles.temporizador}>
-            <CountdownCircleTimer
-                duration={props.duracao}
-                size={120}
-                isPlaying
-                onComplete={props.tempoEsgotado}
-                colors={[
-                    ['#BCE596', 0.33],
-                    ['#F7B801', 0.33],
-                    ['#ED827A', 0.33],
-                ]}>
-                {({ remainingTime }) => remainingTime}
-            </CountdownCircleTimer>
+         
         </div>
     )
 }
